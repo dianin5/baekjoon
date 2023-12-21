d16b458c6db4decb7121fc3caa4494794412df1c
dianin5 <xyg1940@cu.ac.kr>
2023-12-21 19:37:10 +0900

    if문 익숙해지기 단골손님 시험성적

--- 2935.py
+++ 2935.py
@@ -1,12 +1,16 @@
 # 소음
 
-a =int(input())
-b =input()
-c = int(input())
-result=int()
-if b =='*':
-    result = a*c
-elif b=='+':
-    result = a+c
-
+score = int(input())
+result= ""
+if score>= 90 and score<=100:
+    result = 'A'
+elif score>=80 and score<=89:
+    result = 'B'
+elif score >=70 and score<=79:
+    result = 'C'
+elif score >=60 and score<=69:
+    result = 'D'
+else:
+    result = 'F'
+    
 print(result)
