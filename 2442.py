79f2fc07c0a77baab5c5f0b674db93ea958e597d
dianin5 <xyg1940@cu.ac.kr>
2024-01-12 21:33:14 +0900

    int쓰지마!!

--- 2442.py
+++ 2442.py
@@ -1,6 +1,6 @@
 N = int(input())
 for i in range(1,N+1):
         # 1 3 5 7 9
-        print(str(" "*int(N-i))+str("*"*int(i))+(str("*")*int(i-1)))
+        print(str(" "*(N-i))+str("*"*(i))+(str("*")*(i-1)))
         
     
