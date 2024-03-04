862779c04d0ef78d8bbbd63667fc7ef95e25bffb
dianin5 <xyg1940@cu.ac.kr>
2024-03-04 09:09:53 +0900

    짝수찾기

--- 9316.py
+++ 9316.py
@@ -1,3 +1,8 @@
-N=int(input())
-for i in range(1, N+1):
-    print(f"Hello World, Judge {i}!")
+T=int(input())
+for i in range(T):
+    ans=[]
+    a = list(map(int,input().split()))
+    for i in a:
+        if i%2==0:
+            ans.append(i)
+    print(sum(ans),min(ans))
