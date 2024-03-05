ab402cfbaf83c04a9ae6f19f7cc8c1e27c98ca51
dianin5 <xyg1940@cu.ac.kr>
2024-03-05 14:03:37 +0900

    0이 아닐때까지 카운트

---
+++ 1676.py
@@ -0,0 +1,15 @@
+N=int(input())
+temp=1
+for i in range(1,N+1):
+    temp*=i
+str=str(temp)
+ans=list(str)
+ans.reverse()
+count=0
+for i in ans:
+    
+    if i=='0':
+        count+=1
+    else:
+        break
+print(count)
