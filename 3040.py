97e533fc027f7aec5b986d4a1fd722b597190292
dianin5 <xyg1940@cu.ac.kr>
2024-03-19 14:16:45 +0900

    내방식대로 풀어보기

--- 3040.py
+++ 3040.py
@@ -1,12 +1,13 @@
 NaN=[int(input())for _ in range(9)]
+temp=[]
 for i in range(9):
     for j in range(9):
         if i!=j:
             if sum(NaN)-NaN[i]-NaN[j]==100:
-                a=i
-                b=j
+                temp.append(i)
+                temp.append(j)
                 break
-NaN.pop(a)
-NaN.pop(b)
+NaN.pop(temp[0])
+NaN.pop(temp[1]-1)
 for i in NaN:
    print(i)
